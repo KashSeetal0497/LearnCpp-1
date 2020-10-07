1c85e826f7a6dacce769c656fa5f94f561cf46b8
KashSeetal0497 <72173066+KashSeetal0497@users.noreply.github.com>
2020-10-07 17:54:13 +0200

    Update POSsystem.h
    percentage parameter data type change from int to float

--- POSsystem.h
+++ POSsystem.h
@@ -2,8 +2,8 @@
 #ifndef  POSSYSTEM_H
 #define POSSYSTEM_H
 
-  float calculateTip(float gross, int percentage);
-  float calculateNet(float gross, int percentage);
+  float calculateTip(float gross, float percentage);
+  float calculateNet(float gross, float percentage);
   void displayReciept(float gross, float net, float tip);
 
 #endif
